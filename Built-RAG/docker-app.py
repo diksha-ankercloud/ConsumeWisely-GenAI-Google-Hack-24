f41d3a0dfc89468edbeae8fe9cdb3742326b8afd
diksha chakravarty <diksha.chakravarty@ankercloud.com>
2024-09-25 15:20:55 +0530

    update

--- Built-RAG/docker-app.py
+++ Built-RAG/docker-app.py
@@ -12,6 +12,7 @@ from vertexai.generative_models import (
     grounding,
 )
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -33,7 +34,7 @@ def create_genai_model():
 def get_product_info(product):
     """Retrieve product information using Google Search Retrieval."""
     model_ground = create_genai_model()
-    prompt = f"only give me the nutritional (benefits/harms) content regarding the consumption of {product}. Also if there is any recent news regarding if the {product} has some false claims. Also find if the product is from a small business or not."
+    prompt = f"only give me the nutritional (benefits/harms) content regarding the consumption of {product}. Also give information regarding the parent company and its carbon footprint of available of the{product} . Also find if the product is from a small business or not."
     tool = Tool.from_google_search_retrieval(grounding.GoogleSearchRetrieval())
     response = model_ground.generate_content(prompt, tools=[tool])
     return response.candidates[0].content.parts[0].text
@@ -51,13 +52,42 @@ def create_chat_model():
 def create_chat_prompt():
     """Create and return a ChatPromptTemplate instance."""
     return ChatPromptTemplate.from_messages([
-        SystemMessage(content='''You are a well-experienced dietician and nutritionist. You will be given the age, height, weight, and dietary precautions and goals of the person.
-                        You will be given a product and its ingredients to analyze. The goal is to categorize how often the product can be consumed, whether it fits the user's diet, and if it follows sustainability practices. 
-                        Respond based on health analysis using appropriate tags.'''),
+        SystemMessage(content='''You are well experienced dietician and nutritionist. You will be given the Age, height, weight and dietary precautions and goals of the person. 
+                        You will be given a product and its ingredients select one from each category based on the ingredients and health goals of the person only add in the result each category name should be in (#category#) and only answer of each category in (** **).
+                        (Frequency of cunsumption): how frequently can i consume it based on my weight goals:
+                        1. daily consumption
+                        2. weekly consumption
+                        3. monthly consumption
+                        (habit of eating): and as per my diet is this food :
+                        1. nutritional
+                        2. recreations
+                        3.  or regular consumption for me
+
+                        (sustainibility parameters): If the food product follows any of the following:
+                        1. Organic
+                        2. Sustainable: 
+                        3. Supports Small Businesses
+                        4. Animal Cruelty-Free: 
+                        5. Lower carbon footprint
+
+                        (allergy alert): flag alergens if they are related to my allergy otherwise don't flag a warning
+                        (diet type): you need to also flag diet type match this with the input of user info and flag if it is suitable for their dietary type or restrictions(eg vegan/jain/vegetarian/keto) based on user input also flag why not suitable should be a part of the answe in (** **).
+                        (nutrient alert): Alert if there is a higher presence of nutrients desired in low qty (fats, sugar, sodium, calories)
+                        (carbon footprint info): give info about carbon footprint of the product and also give info about the parent company of the product.
+                        Your final answer should only contain tags among one option of each category based on product nutrition analysis.
+                        If the answer is None just drop that category output.
+                        -Finally if the 4 out of 6 categories are suitable for this user add that as highly recommended product.
+                        Your next input will be the info of the person, the name of the product, and screenshot of the ingredients
+                        Make sure you are thorough about all the different tags before printing, all the tags category should be printed, and only the tags
+                        
+            '''),
         HumanMessagePromptTemplate.from_template(
-            template='''The person's info is:
-                Age: {age}, Gender: {gender}, Weight: {weight}, Height: {height}, Dietary Type: {diet_type}, Health Goal: {health_goal}, Allergens: {allergen}.
-                Product info: {product_info_str}, Image: {image_data}'''
+            template='''The person's info is as follows:
+                Person info: I am a {gender} who is {age} years old, my weight is {weight}, my height is {height}, I am {diet_type} and I want to {health_goal}. Allergic to {allergen}.
+                Product info: {product_info_str}
+                'image_url': "data:image/jpeg;base64,{image_data}
+                
+                "'''
         ),
     ])
 
@@ -85,6 +115,7 @@ def analyze_product(user_info, product_info, image_data):
 
     return response.content
 
+
 @app.route('/analyze_product', methods=['POST'])
 def analyze_product_endpoint():
     data = request.get_json()
@@ -111,7 +142,18 @@ def analyze_product_endpoint():
     product_info = get_product_info(product_name)
     result = analyze_product(user_info, product_info, image_data_base64)
 
-    return jsonify({"analysis": result})
+    # Process the result using regex to create the desired JSON format
+    processed_result = {}
+    pattern = r'#\s*(.*?)\s*#:\s*\*\*(.*?)\*\*'
+    matches = re.findall(pattern, result)
+    
+    for key, value in matches:
+        processed_result[key.strip()] = value.strip()
+
+    return jsonify({"analysis": processed_result})
+
+
+
 
 if __name__ == "__main__":
     initialize_environment()
