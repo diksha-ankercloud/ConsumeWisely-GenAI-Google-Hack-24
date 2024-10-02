02d3100fb640287390a9999eac1f3e3bddb12dfb
diksha-ankercloud <156158571+diksha-ankercloud@users.noreply.github.com>
2024-10-02 11:52:50 +0530

    Update header.js

--- src/frontend/src/components/header.js
+++ src/frontend/src/components/header.js
@@ -45,15 +45,14 @@ function Header() {
                 </a>
               </div>
             </div>
-            <div className="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
-              <div className="search-bar-container position-relative">
-                <div className="search-bar row bg-light p-2 my-2 rounded-4">
-                  <div className="col-md-4 d-none d-md-block">
-                    <select className="form-select border-0 bg-transparent">
-                      <option>All Categories</option>
-                      <option>Groceries</option>
-                      <option>Drinks</option>
-                      <option>Chocolates</option>
+            <div className="col-sm-8 offset-sm-1 offset-md-0 col-lg-8 d-none d-lg-block">
+           
+             <div className="search-bar-container position-relative">
+              <div className="search-bar row bg-light p-2 my-2 rounded-4">
+               <div className="col-md-3 d-none d-md-block">
+                 <select className="form-select border-0 bg-transparent">
+                      <option>Search Products</option>
+                      
                     </select>
                   </div>
                   <div className="col-11 col-md-7">
@@ -68,7 +67,8 @@ function Header() {
                         placeholder="Search for more than 20,000 products"
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
-                      />
+                        style={{ width: '105%' }} // Make input take full width
+                     />
                     </form>
                   </div>
 
@@ -213,71 +213,15 @@ function Header() {
 
             <div className="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
               <div className="support-box text-end d-none d-xl-block">
-                <span className="fs-6 text-muted">For Support?</span>
-                <h5 className="mb-0">+980-34984089</h5>
+                
               </div>
 
-              
-              <ul className="d-flex justify-content-end list-unstyled m-0">
-                <li>
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1">
-                    <svg width={24} height={24} viewBox="0 0 24 24">
-                      <use xlinkHref="#user" />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1">
-                    <svg width={24} height={24} viewBox="0 0 24 24">
-                      <use xlinkHref="#heart" />
-                    </svg>
-                  </a>
-                </li>
-                <li className="d-lg-none">
-                  <a
-                    href="#"
-                    className="rounded-circle bg-light p-2 mx-1"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasCart"
-                    aria-controls="offcanvasCart"
-                  >
-                    <svg width={24} height={24} viewBox="0 0 24 24">
-                      <use xlinkHref="#cart" />
-                    </svg>
-                  </a>
-                </li>
-                <li className="d-lg-none">
-                  <a
-                    href="#"
-                    className="rounded-circle bg-light p-2 mx-1"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasSearch"
-                    aria-controls="offcanvasSearch"
-                  >
-                    <svg width={24} height={24} viewBox="0 0 24 24">
-                      <use xlinkHref="#search" />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-              <div className="cart text-end d-none d-lg-block dropdown">
-                <button
-                  className="border-0 bg-transparent d-flex flex-column gap-2 lh-1"
-                  type="button"
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasCart"
-                  aria-controls="offcanvasCart"
-                >
-                  <span className="fs-6 text-muted dropdown-toggle">
-                    Your Cart
-                  </span>
-                  <span className="cart-total fs-5 fw-bold">Rs 1290</span>
-                </button>
+           
               </div>
             </div>
             {/* Other header content */}
           </div>
-        </div>
+        
         <div className="container-fluid">
           <div className="row py-3">
             <div className="d-flex  justify-content-center justify-content-sm-between align-items-center">
@@ -305,31 +249,25 @@ function Header() {
                     />
                   </div>
                   <div className="offcanvas-body">
-                    <select className="filter-categories border-0 mb-0 me-5">
-                      <option>Shop by Departments</option>
-                      <option>Whole Foods</option>
-                      <option>Drinks</option>
-                      <option>Snacks</option>
-                    </select>
                     <ul className="navbar-nav justify-content-end menu-list list-unstyled d-flex gap-md-3 mb-0">
                       <li className="nav-item active">
-                        <a href="/" className="nav-link">
-                          Home
+                        <a href="/" className="nav-link" style={{ fontSize: '16px', fontWeight: 'bold' }}>
+                            Home
                         </a>
                       </li>
                       <li className="nav-item dropdown">
-                        <a href="/shop" className="nav-link">
+                        <a href="/shop" className="nav-link" style={{ fontSize: '16px', fontWeight: 'bold' }}>
                           Shop
                         </a>
                       </li>
-                  
                       <li className="nav-item">
-                        <a href="/about-us" className="nav-link">
+                        <a href="/about-us" className="nav-link" style={{ fontSize: '16px', fontWeight: 'bold' }}>
                           About us
                         </a>
                       </li>
-                    </ul>
+                   </ul>
                   </div>
+
                 </div>
               </nav>
             </div>
